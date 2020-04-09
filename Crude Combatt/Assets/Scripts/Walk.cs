01537edb6bee1c5c03558097ab637eea27ed54a8
MbZ <amobarez00@gmail.com>
2020-04-09 20:04:41 +0430

    Import Dotween and player sprite

---
+++ Crude Combatt/Assets/Scripts/Walk.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Walk : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DOMoveX(5f,5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
